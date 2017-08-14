8df05d1b17ac89fe006118891817800dc5db6f89
Ziga Strgar <ziga_strgar@hotmail.com>
2017-08-14 16:11:40 +0200

    trycatch

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Carbon\Carbon;
 use Closure;
+use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Crypt;
 
@@ -20,15 +21,19 @@ class SetLocale
      */
     public function handle($request, Closure $next)
     {
-        app()->setLocale($lang = Crypt::decrypt($request->cookie('locale')) ? : 'sl');
-        Carbon::setLocale($lang);
+        try {
+            app()->setLocale($lang = Crypt::decrypt($request->cookie('locale')) ? : 'sl');
+            Carbon::setLocale($lang);
 
-        $lc_time = [
-            'sl' => 'sl_SI',
-            'en' => '',
-            'de' => 'de_DE'
-        ];
-        setlocale(LC_TIME, $lc_time[$lang]);
+            $lc_time = [
+                'sl' => 'sl_SI',
+                'en' => '',
+                'de' => 'de_DE'
+            ];
+            setlocale(LC_TIME, $lc_time[$lang]);
+        } catch ( DecryptException $ex ) {
+            
+        }
 
         return $next($request);
     }
