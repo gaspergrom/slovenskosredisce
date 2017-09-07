4ea40123aec0d1c62d7b8aa030e0af48d3767a66
Ziga Strgar <ziga_strgar@hotmail.com>
2017-09-07 16:50:08 +0200

    Storage link for image

--- resources/views/partials/_sponsor_small.blade.php
+++ resources/views/partials/_sponsor_small.blade.php
@@ -1,3 +1,3 @@
 <div class="item flex flex--middle flex--center"><img
-            src="{{ asset($sponsor_logo) }}"
+            src="{{ asset('storage/'.$sponsor_logo) }}"
             style="height: 90px; width: auto;"></div>
