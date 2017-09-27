123ef34c50f01c832faf170264ab6139543117aa
Ziga Strgar <ziga_strgar@hotmail.com>
2017-09-27 13:40:35 +0200

    Images after gallery title

--- resources/views/admin/gallery/_form.blade.php
+++ resources/views/admin/gallery/_form.blade.php
@@ -1,14 +1,3 @@
-<div class="flex flex--center">
-    <label for="file" class="flex flex--middle mb20">
-        Dodaj slike
-        {!! Form::file('images[]', ['multiple' => true, 'id' => 'file']) !!}
-        <span type="button" class="btn btn--round btn__default flex flex--middle ml20">
-                                <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-                                    <path fill="#ffffff" d="M9,16V10H5L12,3L19,10H15V16H9M5,20V18H19V20H5Z"/>
-                                </svg>
-                            </span>
-    </label>
-</div>
 @include('admin._flags')
 <div>
     <div id="slo">
@@ -23,6 +12,17 @@
         <h4>Ger</h4>
         {!! Form::text('name[de]', null, ['class' => 'box mb15', 'placeholder' => 'Naslov galerije (ger)']) !!}
     </div>
+</div>
+<div class="flex flex--center">
+    <label for="file" class="flex flex--middle mb20">
+        Dodaj slike
+        {!! Form::file('images[]', ['multiple' => true, 'id' => 'file']) !!}
+        <span type="button" class="btn btn--round btn__default flex flex--middle ml20">
+                                <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                                    <path fill="#ffffff" d="M9,16V10H5L12,3L19,10H15V16H9M5,20V18H19V20H5Z"/>
+                                </svg>
+                            </span>
+    </label>
 </div>
 <div class="flex flex--center">
     <button class="btn btn__default btn--round">
