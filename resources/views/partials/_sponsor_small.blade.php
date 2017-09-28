01ffabab2205005d12524e25c7080d63ff84aa04
Ziga Strgar <ziga_strgar@hotmail.com>
2017-09-28 12:29:29 +0200

    new sponsors slider

--- resources/views/partials/_sponsor_small.blade.php
+++ resources/views/partials/_sponsor_small.blade.php
@@ -1,5 +1,12 @@
 <a href="{{ $sponsor->url }}" target="_blank" rel="noreferrer noopener">
-    <div class="item flex flex--middle flex--center"><img
-                src="{{ asset('storage/'.$sponsor->logo) }}"
-                style="height: 90px; width: auto;"></div>
+    <div class="item flex flex--middle flex--center quadric bg--white box">
+        <div class="flex flex--center flex--middle" style="position: absolute; top: 0;left: 0; right: 0; bottom: 0">
+            <img src="{{ asset('storage/'.$sponsor->logo) }}" style="height: 90px; width: auto;">
+        </div>
+        <div class="overlay pt10 pb10 pl10 pr10 flex flex--middle flex--center">
+            <div class="flex flex--column">
+                <h4 class="text-center text--white">{{ $sponsor->name }}</h4>
+            </div>
+        </div>
+    </div>
 </a>
