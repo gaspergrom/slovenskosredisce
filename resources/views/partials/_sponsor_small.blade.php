e37b94e0febb8e22634fea8c6bf61cf25dea21fa
Ziga Strgar <ziga_strgar@hotmail.com>
2017-09-28 12:33:17 +0200

    Let's see

--- resources/views/partials/_sponsor_small.blade.php
+++ resources/views/partials/_sponsor_small.blade.php
@@ -1,7 +1,9 @@
 <a href="{{ $sponsor->url }}" target="_blank" rel="noreferrer noopener">
     <div class="item flex flex--middle flex--center quadric bg--white box">
         <div class="flex flex--center flex--middle" style="position: absolute; top: 0;left: 0; right: 0; bottom: 0">
-            <img src="{{ asset('storage/'.$sponsor->logo) }}" style="height: 90px; width: auto;">
+            <div>
+                <img src="{{ asset('storage/'.$sponsor->logo) }}" style="height: 90px; width: auto;">
+            </div>
         </div>
         <div class="overlay pt10 pb10 pl10 pr10 flex flex--middle flex--center">
             <div class="flex flex--column">
