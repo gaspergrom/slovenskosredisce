fbeb46fbd8ddfdaece35048dba7d164e1f0c2bcc
Ziga Strgar <ziga_strgar@hotmail.com>
2017-09-28 13:22:16 +0200

    What about now?

--- resources/views/partials/_sponsor_small.blade.php
+++ resources/views/partials/_sponsor_small.blade.php
@@ -2,7 +2,7 @@
     <div class="item flex flex--middle flex--center quadric bg--white box">
         <div class="flex flex--center flex--middle" style="position: absolute; top: 0;left: 0; right: 0; bottom: 0">
             <div>
-                <img src="{{ asset('storage/'.$sponsor->logo) }}" style="height: 90px; width: auto;">
+                <img src="{{ asset('storage/'.$sponsor->logo) }}" style="height: auto; width: 100%;">
             </div>
         </div>
         <div class="overlay pt10 pb10 pl10 pr10 flex flex--middle flex--center">
