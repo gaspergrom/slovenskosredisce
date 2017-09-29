c72e3127f41d9a59b3bad4c875f949d9eb72e6c0
Ziga Strgar <ziga_strgar@hotmail.com>
2017-09-29 15:54:31 +0200

    Edit now have error partial

--- resources/views/admin/leadership/edit.blade.php
+++ resources/views/admin/leadership/edit.blade.php
@@ -5,6 +5,7 @@
         <div class="container pt150 pb60">
             <h1 class="text-center">{{ $leadership['name']['sl']}}</h1>
             <hr>
+            @include('partials._errors')
             <div class="flex flex--center">
                 <div class="col-md-8">
                     {!! Form::model($leadership, ['route' => ['vodstvo.update', $leadership['id']], 'files' => true, 'method' => 'PATCH']) !!}
--- resources/views/admin/sponsors/edit.blade.php
+++ resources/views/admin/sponsors/edit.blade.php
@@ -5,6 +5,7 @@
         <div class="container pt150 pb60">
             <h1 class="text-center">Urejanje sponzorja</h1>
             <hr>
+            @include('partials._errors')
             <div class="flex flex--center">
                 <div class="col-md-8">
                     {!! Form::model($sponsor, ['route' => ['sponzorji.update', $sponsor['id']], 'files' => true, 'method' => 'PATCH']) !!}
