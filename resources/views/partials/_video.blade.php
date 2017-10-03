e49242f1a2ce254e6f3c37a83b88d9ea7700b7a0
Ziga Strgar <ziga_strgar@hotmail.com>
2017-10-03 12:51:55 +0200

    Video matching frontend

--- resources/views/partials/_video.blade.php
+++ resources/views/partials/_video.blade.php
@@ -1,4 +1,5 @@
 <div class="col-md-4 pb30">
+    <p>{{ $video->name }}</p>
     <div class="quadric--medium bg--image" data-video="{{ asset('storage/'.$video->path) }}"
-         style="background-image: url('{{ asset('images/bg2.jpg') }}')"></div>
+         style="background-image: url('{{ asset($video->image) }}')"></div>
 </div>
