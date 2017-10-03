d2d8470947b086e9ae45b4d06a538c6f0683fbfe
Ziga Strgar <ziga_strgar@hotmail.com>
2017-10-03 12:51:06 +0200

    video name shown

--- resources/views/admin/videos/_video.blade.php
+++ resources/views/admin/videos/_video.blade.php
@@ -1,4 +1,5 @@
 <div data-video="{{ $video->id }}" class="col-md-4 pb30">
+    <p>{{ $video->name }}</p>
     <div class="quadric--medium bg--image" data-video="{{ $video->path }}"
          style="background-image: url('{{ asset($video->image) }}')"></div>
     <a href="{{ route('videoposnetki.edit', $video->id) }}">Uredi</a>
