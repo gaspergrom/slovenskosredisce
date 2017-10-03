02dba472a78a78f2654cbe4626c4e8673fff375b
Ziga Strgar <ziga_strgar@hotmail.com>
2017-10-03 20:40:00 +0200

    Path fix

--- resources/views/admin/videos/_video.blade.php
+++ resources/views/admin/videos/_video.blade.php
@@ -1,7 +1,7 @@
 <div data-video="{{ $video->id }}" class="col-md-4 pb30">
     <p>{{ $video->name }}</p>
-    <div class="quadric--medium bg--image" data-video="{{ $video->path }}"
-         style="background-image: url('{{ asset($video->image) }}')"></div>
+    <div class="quadric--medium bg--image" data-video="{{ 'storage/'.$video->path }}"
+         style="background-image: url('{{ asset('storage/'.$video->image) }}')"></div>
     <a href="{{ route('videoposnetki.edit', $video->id) }}">Uredi</a>
     <button onclick="deleteVideo({{ $video->id }});">Izbriši</button>
 </div>
--- resources/views/partials/_video.blade.php
+++ resources/views/partials/_video.blade.php
@@ -1,5 +1,5 @@
 <div class="col-md-4 pb30">
     <p>{{ $video->name }}</p>
     <div class="quadric--medium bg--image" data-video="{{ asset('storage/'.$video->path) }}"
-         style="background-image: url('{{ asset($video->image) }}')"></div>
+         style="background-image: url('{{ asset('storage/'.$video->image) }}')"></div>
 </div>
