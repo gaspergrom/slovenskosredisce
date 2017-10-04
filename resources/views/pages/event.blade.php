a0870f8f49db6221a0bec2a2cb03fd2b31e0c702
Ziga Strgar <ziga_strgar@hotmail.com>
2017-10-04 12:39:51 +0200

    Event location and date in center

--- resources/views/pages/event.blade.php
+++ resources/views/pages/event.blade.php
@@ -13,7 +13,7 @@
                                 @endif
                                 <h1 class="text-center h2">{{ $event->name }}</h1>
                                 <hr>
-                                <div class="flex flex--middle pb30">
+                                <div class="flex flex--middle flex--center pb30">
                                     <svg style="width:24px;height:24px" viewBox="0 0 24 24">
                                         <path fill="#4dbc46"
                                               d="M19,19H5V8H19M16,1V3H8V1H6V3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3H18V1"/>
