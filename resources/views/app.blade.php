ccdfa2ab4d1ccafd7586d925101503eac526a3c0
Ziga Strgar <ziga_strgar@hotmail.com>
2017-10-17 11:01:33 +0200

    Remove of event submission

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -101,7 +101,6 @@
             </div>
         </div>
     </footer>
-
 </main>
 <script src="https://code.jquery.com/jquery-2.2.3.min.js"
         integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo=" crossorigin></script>
